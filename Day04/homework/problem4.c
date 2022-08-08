74aa0e956a6ca4cf6f50260e74e4452afc8fbff6
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-08-08 07:16:23 -0500

    Update problem4.c

--- Day04/homework/problem4.c
+++ Day04/homework/problem4.c
@@ -6,6 +6,11 @@ int main() {
 
     printf("num1 XOR num2 XOR num3 is ");
     printf("%d\n", (num1^num2)^num3);
+    
+    // Compile:
+    // gcc problem4.c
+    // Read the numbers from a text file:
+    // cat numbers.txt | ./a.out
 
     return 0;
 }
