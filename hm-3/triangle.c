99402a9e9435243812f9f9d6e66c9767257c5d88
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-08-21 13:47:18 +0300

    Update triangle.c
    Updated and bug fixes

--- hm-3/triangle.c
+++ hm-3/triangle.c
@@ -6,13 +6,15 @@ int triangle(double a, double b, double c, double *S, double* P);
 int main() {
 
     double a, b, c, area, per;
-    scanf("%lf %lf %lf", &a, &b, &c);
-
-    triangle(a, b, c, &area, &per);
-
-    printf("S = %.2f\n", area);
-    printf("P = %.2f", per);
-
+    while (scanf("%lf %lf %lf", &a, &b, &c) != EOF) {
+        if (!triangle(a, b, c, &area, &per)) {
+            printf("S = %.2f\n", area);
+            printf("P = %.2f\n", per);
+        } else {
+            printf("Invalid triangle sides!\n");
+        }
+    }
+    
     return 0;
 }
 
@@ -20,10 +22,11 @@ int triangle(double a, double b, double c, double *S, double* P) {
     if (a <= 0 || b <= 0 || c <= 0)
         return -1;
 
-    
-    
-    *S = (1/4) * sqrt(4 * pow(a*b, 2)) - 
-    pow((a*a + b*b - c*c), 2);
+    if ((a > (b + c)) || (b > (a + c)) || (c > (a + b)))
+        return -1;
+
+    *S = 0.25 * sqrtf((4 * powf((a*b), 2)) - 
+    powf((a*a + b*b - c*c), 2));
 
     *P = a + b +c;
 
