4ec5a139959fc64a366cb4aa66fdae0a84202c48
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-08-21 08:18:31 -0500

    Create problem6sample.c

---
+++ hm-3/problem6sample.c
@@ -0,0 +1,28 @@
+int main(void)
+{
+    uint32_t attendance = 0;
+    int option;
+    while (1)
+    {
+        printf("1. Set attendance\n");
+        printf("2. Clear attendance\n");
+        printf("3. Attendance info\n");
+        printf("4. Change attendance\n");
+        printf("5. Exit\n");
+        scanf("%d", &option);
+        if (option == 1)
+        {
+             // to do
+        }
+        else if (option == 2)
+        {
+            // to do
+        }
+        else if (option == 3)
+        {
+           // to do
+        }
+        // to do
+    }
+    return 0;
+}
