572bc1c6e05496e6c23203cc8e40cda1f443e0cb
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-08-24 04:40:27 -0500

    Add files via upload

---
+++ hw-4/sumDigits.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+unsigned sumArrayDigits(const int arr[], size_t n);
+
+int main() {
+    int arr[] = {12, 34, 5, 70};
+    unsigned sumOfDigits = sumArrayDigits(arr, 4);
+    printf("%u\n", sumOfDigits);
+
+    return 0;
+}
+
+unsigned sumArrayDigits(const int arr[], size_t n) {
+    unsigned sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        unsigned num = arr[i];
+        unsigned digits = 0;
+        while (num) {
+            digits += (num % 10);
+            num /= 10;
+        }
+        sum += digits;
+    }
+
+    return sum;
+}
