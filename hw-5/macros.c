4cf106b8ec2cc2ce463a867211555f08be58c3d3
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-09-01 19:25:24 +0300

    Added SWAP

--- hw-5/macros.c
+++ hw-5/macros.c
@@ -4,6 +4,7 @@
 #define MAX_OF_THREE(x, y, z) MAX(MAX(x,y), (z))
 #define MIN(x,y) ((x) < (y) ? (x) : (y))
 #define MIN_OF_THREE(x, y, z) MIN(MIN(x,y), (z))
+#define SWAP(a,b) ((a) ^= (b) ^= (a) ^= (b))
 
 int main() {
     int a, b ,c;
@@ -13,6 +14,9 @@ int main() {
     printf("Biggest of three nums is: %d\n", MAX_OF_THREE(a, b, c));
     printf("Smallest of three nums is: %d\n", MIN_OF_THREE(a, b, c));
 
+    printf("a and b before swapping: %d, %d\n", a, b);
+    SWAP(a, b);
+    printf("a and b after swapping: %d, %d\n", a, b);
 
     return 0;
 }
