45f535cd7c5bc0e90c49075c7bea24ee61a43502
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-09-03 11:55:30 +0300

    Update macros.c

--- hw-5/macros.c
+++ hw-5/macros.c
@@ -4,6 +4,10 @@
 #define MAX_OF_THREE(x, y, z) MAX(MAX(x,y), (z))
 #define MIN(x,y) ((x) < (y) ? (x) : (y))
 #define MIN_OF_THREE(x, y, z) MIN(MIN(x,y), (z))
+#define SETBIT(mask, bit) (mask |= (1ull << (bit)))
+#define CLEARBIT(mask, bit) (mask &= ~(1ull << (bit)))
+#define INVERSEBIT(mask, bit) (mask ^= (1ull << (bit)))
+#define CHECKBIT(mask, bit) !!(mask & (1ull << (bit)))
 #define SWAP(a,b) ((a) ^= (b) ^= (a) ^= (b))
 
 int main() {
@@ -17,6 +21,16 @@ int main() {
     printf("a and b before swapping: %d, %d\n", a, b);
     SWAP(a, b);
     printf("a and b after swapping: %d, %d\n", a, b);
+
+    int mask = 0;
+    SETBIT(mask, 3);
+    printf("Bit at position 3: %u\n", CHECKBIT(mask, 3));
+    INVERSEBIT(mask, 0);
+    printf("Bit at position 0: %u\n", CHECKBIT(mask, 0));
+    CLEARBIT(mask, 3);
+    printf("Bit at position 3: %u\n", CHECKBIT(mask, 3));
+    printf("mask %d\n", mask);
+
 
     return 0;
 }
