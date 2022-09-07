8a8d25d1c3181ed76228e94a00e9933c659e5213
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-09-07 07:18:50 -0500

    Add files via upload

---
+++ hw-6-7/problem1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+
+unsigned char CheckBit(uint16_t uValue, unsigned bit);
+unsigned char isOneBitSet (uint16_t uValue);
+
+int main (){
+    uint16_t num = 0x0400;
+    uint16_t num2 = 0x0600;
+
+    if (isOneBitSet(num))
+        printf("0x01\n");
+    else
+        printf("0x00\n");
+
+    if (isOneBitSet(num2))
+        printf("0x01\n");
+    else
+        printf("0x00\n");
+
+    return 0;
+}
+
+unsigned char CheckBit(uint16_t uValue, unsigned bit) {
+    return !!(uValue & (1 << bit));
+}
+
+unsigned char isOneBitSet (uint16_t uValue) {
+    size_t counter = 0;
+    unsigned char res;
+
+    for (int i = 15; i >= 0; i--) {
+        res = CheckBit(uValue, i);
+        if (res)
+            counter++;
+    }
+    if (counter == 1)
+        return 1;
+    else
+        return 0;
+}
+
