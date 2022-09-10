49f69ae8aa88372ee1c4fbc2cd03057459fc24a5
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-09-10 13:35:53 +0300

    Update sumDigits.c

--- final-exam/problem2/sumDigits.c
+++ final-exam/problem2/sumDigits.c
@@ -3,11 +3,17 @@
 unsigned sumArrayDigits(const int *arr, size_t n);
 
 int main() {
+    int main() {
     int arr[] = {12, 34, 5, 70};
     unsigned sumOfDigits = sumArrayDigits(arr, 4);
     printf("%u\n", sumOfDigits);
 
+    int arr2[] = {0, 0, 0, 0, 0};
+    unsigned sumOfDigits2 = sumArrayDigits(arr2, 5);
+    printf("%u\n", sumOfDigits2);
+
     return 0;
+}
 }
 
 unsigned sumArrayDigits(const int *arr, size_t n) {
