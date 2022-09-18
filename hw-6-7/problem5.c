672131d2a510631c7dc028cac681ab939adff345
Albena <albena.y.krasteva@gmail.com>
2022-09-19 01:49:05 +0300

    Ring buffer queue

---
+++ hw-6-7/problem5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX_SIZE 5
+
+typedef struct {
+    int * const ringBuff;
+    int top;
+    int bottom;
+    const int size;
+} buffer_t;
+
+int main() {
+
+    return 0;
+}
