129559487ee1443b1b844ec830876b60fb7dc6e6
Albena Krasteva <70164850+ayk-dev@users.noreply.github.com>
2022-09-19 18:59:02 +0300

    Update problem5.c
    Push is not working

--- hw-6-7/problem5.c
+++ hw-6-7/problem5.c
@@ -1,61 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+	
 #define MAX_SIZE 5
-
+	
 typedef struct {
-    int * const ringBuff;
-    int top;
-    int bottom;
-    const int maxSize;
+	int * const ringBuff;
+	int top;
+	int bottom;
+	int maxSize;
 } buffer_t;
-
-int isEmpty(int head, int tail);
-int isFull(int head, int tail);
+	
+int isEmpty(buffer_t *rb);
+int isFull(buffer_t *rb);
 void Push(buffer_t *rb, int newValue);
 int Pop(buffer_t *rb, int *value);
-
+void Free(buffer_t *rb);
+	
 int main() {
-    buffer_t circBuff;
+	buffer_t *circBuff = malloc(MAX_SIZE * sizeof(int));
+	circBuff->maxSize = MAX_SIZE;
+    circBuff->top = 0;
+    circBuff->bottom = 0;
+
+    printf("%d\n", isEmpty(circBuff));
+    printf("%d\n", isFull(circBuff));
     
-    
-    
+    // Push function is not working
+    Push(circBuff, 5);
+    Push(circBuff, 10);
+    Push(circBuff, 15);
 
-    return 0;
+    printf("%d\n", circBuff->top);
+    printf("%d\n", circBuff->bottom);
+	
+	Free(circBuff);
+	return 0;
 }
-
-int isEmpty(int head, int tail) {
-    return abs(head - tail);
+	
+int isEmpty(buffer_t *rb) {
+	return abs(rb->top - rb->bottom);
 }
-
-int isFull(int head, int tail) {
-    return abs((head + 1) - tail);
+	
+int isFull(buffer_t *rb) {
+	return abs((rb->top + 1) - rb->bottom);
 }
-
+	
 void Push(buffer_t *rb, int newValue) {
-    int next;
-    next = rb->top + 1;
-    
-    if (next >= rb->maxSize)
-        next = 0;
-    
-    if (!isFull(rb->top, rb->bottom)) {
-        rb->ringBuff[rb->top] = newValue;
-        rb->top = next;
-    }
+	int next;
+	next = rb->top + 1;
+	
+	if (next >= rb->maxSize)
+	    next = 0;
+	
+	if (!isFull(rb)) {
+	    rb->ringBuff[rb->top] = newValue;
+	    rb->top = next;
+	}
+}
+	
+int Pop(buffer_t *rb, int *value) {
+	int next;
+	
+	if (isEmpty(rb))
+	return -1;
+	
+	next = rb->bottom + 1;
+	if (next >= rb->maxSize)
+	next = 0;
+	
+	*value = rb->ringBuff[rb->bottom];
+	rb->bottom = next;
+	return 0;
 }
 
-int Pop(buffer_t *rb, int *value) {
-    int next;
-
-    if (isEmpty(rb->top, rb->bottom))
-        return -1;
-
-    next = rb->bottom + 1;
-    if (next >= rb->maxSize)
-        next = 0;
-
-    *value = rb->ringBuff[rb->bottom];
-    rb->bottom = next;
-    return 0;
+void Free(buffer_t *rb) {
+    free(rb->ringBuff);
+    free(rb);
 }
